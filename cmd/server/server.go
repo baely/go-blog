c273b8683785d58599f111f986f7e5d34eb382fc
baely <bailey.butler.234@gmail.com>
2022-03-01 09:31:19 +1100

    Use a router

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,6 +5,8 @@ import (
 	"go-blog/pkg/blog"
 	"go-blog/pkg/util"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 type serverData struct {
@@ -34,6 +36,37 @@ func loadServerData(authorsFile string, postsFile string) (serverData, error) {
 	return data, nil
 }
 
+func (s *Server) Run(authorsFile string, postsFile string) error {
+	if s.port == "" {
+		s.port = "8080"
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/authors", s.serveAuthors)
+	r.Get("/posts", s.serverPosts)
+
+	if err := s.init(authorsFile, postsFile); err != nil {
+		return err
+	}
+
+	fmt.Println("Server is running...")
+
+	if err := http.ListenAndServe(s.address+":"+s.port, r); err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func (s *Server) init(authorsFile string, postsFile string) error {
+	data, err := loadServerData(authorsFile, postsFile)
+	if err != nil {
+		return err
+	}
+	s.data = data
+}
+
 func (s *Server) serveAuthors(w http.ResponseWriter, r *http.Request) {
 	for i, author := range s.data.authors {
 		if _, err := fmt.Fprintf(w, "Author #%d: [%s]\n", i, author.DisplayName()); err != nil {
@@ -48,30 +81,4 @@ func (s *Server) serverPosts(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(err)
 		}
 	}
-}
-
-func (s *Server) Run(authorsFile string, postsFile string) error {
-	if s.address == "" {
-		s.address = "0.0.0.0"
-	}
-	if s.port == "" {
-		s.port = "8080"
-	}
-
-	data, err := loadServerData(authorsFile, postsFile)
-	if err != nil {
-		return err
-	}
-	s.data = data
-
-	http.HandleFunc("/authors", s.serveAuthors)
-	http.HandleFunc("/posts", s.serverPosts)
-
-	fmt.Println("Server is running...")
-
-	if err := http.ListenAndServe(s.address+":"+s.port, nil); err != nil {
-		return err
-	}
-
-	return nil
 }
